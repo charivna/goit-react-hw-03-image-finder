c1f3303c9e5cf7e05e87dcfcdfc4e47206017c4b
charivna <124678194+charivna@users.noreply.github.com>
2023-08-29 11:57:45 +0200

    push

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+
+export class App extends Component() {
+  state = {};
+
+  render() {
+    return (
+      <>
+        <h1>helll</h1>
+      </>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,27 @@
+import { Component } from 'react';
+
+export class Searchbar extends Component() {
+  state = {};
+
+  render() {
+    return (
+      <>
+        <header class="searchbar">
+          <form class="form">
+            <button type="submit" class="button">
+              <span class="button-label">Search</span>
+            </button>
+
+            <input
+              class="input"
+              type="text"
+              autocomplete="off"
+              autofocus
+              placeholder="Search images and photos"
+            />
+          </form>
+        </header>
+      </>
+    );
+  }
+}

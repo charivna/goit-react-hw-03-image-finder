43058db2f70d7fe9fe5f897623db93a000fe50d5
charivna <124678194+charivna@users.noreply.github.com>
2023-08-29 11:59:56 +0200

    problem

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,27 +1,27 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 
-export class Searchbar extends Component() {
-  state = {};
+// export class Searchbar extends Component() {
+//   state = {};
 
-  render() {
-    return (
-      <>
-        <header class="searchbar">
-          <form class="form">
-            <button type="submit" class="button">
-              <span class="button-label">Search</span>
-            </button>
+//   render() {
+//     return (
+//       <>
+//         <header class="searchbar">
+//           <form class="form">
+//             <button type="submit" class="button">
+//               <span class="button-label">Search</span>
+//             </button>
 
-            <input
-              class="input"
-              type="text"
-              autocomplete="off"
-              autofocus
-              placeholder="Search images and photos"
-            />
-          </form>
-        </header>
-      </>
-    );
-  }
-}
+//             <input
+//               class="input"
+//               type="text"
+//               autocomplete="off"
+//               autofocus
+//               placeholder="Search images and photos"
+//             />
+//           </form>
+//         </header>
+//       </>
+//     );
+//   }
+// }

a57f81c7323994735a4c1aade49a5ef84c26839e
charivna <124678194+charivna@users.noreply.github.com>
2023-09-01 14:03:32 +0200

    fix

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,19 +2,21 @@ import { ImageGalleryItem } from './ImageGalleryItem';
 import { Gallery } from './ImageGallery.styled';
 
 export const ImageGallery = ({ images, onClick }) => {
-  <Gallery>
-    {images.map(({ id, webformatURL, largeImageURL, tags }) => {
-      return (
-        <ImageGalleryItem
-          key={id}
-          src={webformatURL}
-          dataSrc={largeImageURL}
-          tags={tags}
-          onClick={onClick}
-        />
-      );
-    })}
-  </Gallery>;
+  return (
+    <Gallery>
+      {images.map(({ id, webformatURL, largeImageURL, tags }) => {
+        return (
+          <ImageGalleryItem
+            key={id}
+            src={webformatURL}
+            dataSrc={largeImageURL}
+            tags={tags}
+            onClick={onClick}
+          />
+        );
+      })}
+    </Gallery>
+  );
 };
 
 // export class ImageGallery extends Component {
